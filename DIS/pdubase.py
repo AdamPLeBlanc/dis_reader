346588ce261cd2862946982186dc55ec41e954da
Adam LeBlanc <adam.p.leblanc@boeing.com>
2016-01-20 12:42:13 -0800

    updated organization and updated pdu architecture

--- DIS/pdubase.py
+++ DIS/pdubase.py
@@ -1,60 +1,92 @@
 #!/usr/bin/env python
 import struct
-ENDIANESS = {'LITTLE':'<','BIG':'>'}  #endianess struct format character mapping
-class Rec(object): 
-    Length = 0
 
+ENDIANESS = {'LITTLE':'<','BIG':'>'}  #endianess struct format character mapping
+
+
+
+class Record(object): 
+    #Length = 0
     def __init__(self,packet=None,endianess='LITTLE'):
         self._packet = packet
         self._endianess = endianess
+        self._fmt = None
+    
+    @property
+    def _fullFmt(self):
+        return "{0}{1}".format(ENDIANESS[self._endianess],self._fmt)
+
     def Pack(self):
         pass
 
+    def Size(self):
+        retVal = 0
+        if self._fmt:
+            retVal = struct.calcsize(self._fmt)
+        return retVal
+
+class CustomRecord(Record):
+    def __init__(self,fmt,packet=None,endianess='LITTLE',valDecoder=None):
+        super(CustomRecord,self).__init__(packet,endianess)
+        self._fmt = fmt
+        self._valDecoder = valDecoder
+        if self._valDecoder:
+            self.Value = self._valDecoder(struct.unpack_from(self._fullFmt,packet))
+        else:
+            self.Value = None
+
+    def Pack(self):
+        return struct.pack(self._fullFmt,self.Value)
+
+    def __str__(self):
+        return str(self.Value)
+
 '''class definition for Pdu Status Record'''
-class PduStatusRecord(Rec):
-    Length = 1
+class PduStatusRecord(Record):
+    #Length = 1
     #@remark - see pduStatusRules.png to visualize _rules
     _rules = {'TEI':[1,31,32,41,67] + range(23,29), 'LVC':range(1,73),'CEI':range(1,72),
                      'FTI':[2],'DTI':[3],'RAI':[25,26,27],'IAI':[31,32],'ISM':[28],'AII':[28]}
     def __init__(self,pduType,packet=None,endianess='LITTLE'):
-        Rec.__init__(self,packet)
+        Record.__init__(self,packet,endianess)
+        self._fmt = 'B'
         if pduType < 1 or pduType > 255:
             raise ValueError
         self.PduType = pduType
         if not packet:
-            self.Value = 0
+            self._value = 0
         else:
-            self.Value = struct.unpack_from('B',packet)[0]
+            self._value = struct.unpack_from(self._fullFmt,packet)[0]
     @property
     def TEI(self):  #transfered entity indicator - no difference:0,  difference:1
-        return self.Value & 0x01 if self.PduType in self._rules['TEI'] else None
+        return self._value & 0x01 if self.PduType in self._rules['TEI'] else None
     @property
     def LVC(self):  #LVC indicator - live:0, virtual:1, constructive:2
-        return (self.Value >> 1) & 0x03 if self.PduType in self._rules['LVC'] else None
+        return (self._value >> 1) & 0x03 if self.PduType in self._rules['LVC'] else None
     @property
     def CEI(self):  #coupled extension indicator - not coupled:0, coupled(1)
-        return (self.Value >> 3) & 0x01 if self.PduType in self._rules['CEI'] else None
+        return (self._value >> 3) & 0x01 if self.PduType in self._rules['CEI'] else None
     @property
     def FTI(self):  #fire type indicator - munition:0, expendable:1
-        return (self.Value >> 4) & 0x01 if self.PduType in self._rules['FTI'] else None
+        return (self._value >> 4) & 0x01 if self.PduType in self._rules['FTI'] else None
     @property
     def DTI(self):  #detonation type indicator - munition:0, expendable:1, non-munition:2
-        return (self.Value >> 5) & 0x03 if self.PduType in self._rules['DTI'] else None
+        return (self._value >> 5) & 0x03 if self.PduType in self._rules['DTI'] else None
     @property
     def RAI(self):  #radio attached indicator - no statement:0, unattached:1, attached:2
-        return (self.Value >> 5) & 0x03 if self.PduType in self._rules['RAI'] else None
+        return (self._value >> 5) & 0x03 if self.PduType in self._rules['RAI'] else None
     @property
     def IAI(self):  #intercom attached indicator - no statement:0, unattached:1, attached:2
-        return (self.Value >> 5) & 0x03 if self.PduType in self._rules['IAI'] else None
+        return (self._value >> 5) & 0x03 if self.PduType in self._rules['IAI'] else None
     @property
     def ISM(self):  #iff sim mode - regeneration:0, interactive:1
-        return (self.Value >> 4) & 0x01 if self.PduType in self._rules['ISM'] else None
+        return (self._value >> 4) & 0x01 if self.PduType in self._rules['ISM'] else None
     @property
     def AII(self):  #active interogation indicator - not active:0, active:1
-        return (self.Value >> 5) & 0x01 if self.PduType in self._rules['AII'] else None
+        return (self._value >> 5) & 0x01 if self.PduType in self._rules['AII'] else None
 
     def Pack(self):
-        return self._packet
+        return struct.pack(self._fullFmt,self._value)
 
     def __str__(self):
         return \
@@ -69,19 +101,19 @@ class PduStatusRecord(Rec):
         "AII: %r" % (self.TEI, self.LVC, self.CEI, self.FTI, self.DTI, self.RAI, self.IAI, self.ISM, self.AII)
 
 '''class definition for PDU Header Record class '''
-class PduHeaderRecord(Rec):
+class PduHeaderRecord(Record):
     protocolVersionRange = range(7)            #protocolVersion range is 0-6
     pduTypeRange = range(68) + range(129,136)  #pduType range is 0-67 & 129-135
     protocolFamilyRange = range(13) + [129]    #protocolFamily range is 0-12 & 129
-    Length = 12
+    #Length = 12
     def __init__(self,packet=None,endianess='LITTLE'):
-        Rec.__init__(self,packet,endianess)
-        self._fmt = ENDIANESS[self._endianess] + 'BBBBIHBB'
+        Record.__init__(self,packet,endianess)
+        self._fmt = 'BBBBIHBB'
         if not packet:
             self._pVersion,self._exId,self._pduType,self._pFam,self._tStamp,self._pduStat,self._totalLen = 0,0,0,0,0,0,0
         if packet:
             self.ProtocolVersion, self.ExerciseId,self.PduType,self.ProtocolFamily,self.Timestamp,self.TotalLength,self.PduStatus,self.Padding = \
-                struct.unpack_from(self._fmt,packet)
+                struct.unpack_from(self._fullFmt,packet)
 
     @property
     def ProtocolVersion(self):
@@ -133,7 +165,7 @@ class PduHeaderRecord(Rec):
         self._pduStat = value
 
     def Pack(self):
-            return struct.pack(self._fmt,self.ProtocolVersion,self.ExerciseId, \
+            return struct.pack(self._fullFmt,self.ProtocolVersion,self.ExerciseId, \
                                self.PduType,self.ProtocolFamily,self.Timestamp,self.TotalLength,self._pduStat,self.Padding)
     def __str__(self):
         return \
@@ -147,38 +179,39 @@ class PduHeaderRecord(Rec):
                                 self.Timestamp,self.TotalLength,'\n  '.join(str(self.PduStatus).splitlines()))
 
 '''class definition for Force Identification Record'''
-class ForceIdRecord(Rec):
-    Length = 1
+class ForceIdRecord(Record):
+    #Length = 1
 
     def __init__(self,packet=None,endianess='LITTLE'):
-        Rec.__init__(self,packet)
+        Record.__init__(self,packet)
+        self._fmt = 'B'
         if not packet:
             self.Value = 0
         else:
-            self.Value = struct.unpack_from('B',packet)[0]
+            self.Value = struct.unpack_from(self._fullFmt,packet)[0]
         if self.Value < 0 or self.Value > 30:
             raise ValueError
 
     def Pack(self):
-        return struct.pack('B',self.Value)
+        return struct.pack(self._fullFmt,self.Value)
 
     def __str__(self):
         return 'Value: %r' % self.Value
 
 '''class definition for Entity Id Record'''
-class EntityIdRecord(Rec):
-    Length = 6
+class EntityIdRecord(Record):
+    #Length = 6
 
     def __init__(self,packet=None,endianess='LITTLE'):
-        Rec.__init__(self,packet,endianess)
-        self._fmt = ENDIANESS[endianess] + 'HHH'
+        Record.__init__(self,packet,endianess)
+        self._fmt = 'HHH'
         if not packet:
             self.SiteNum, self.AppNum, self.EntityNum = 0,0,0
         else:
-            self.SiteNum,self.AppNum,self.EntityNum = struct.unpack_from(self._fmt,packet)
+            self.SiteNum,self.AppNum,self.EntityNum = struct.unpack_from(self._fullFmt,packet)
 
     def Pack(self):
-        return struct.pack(self._fmt,self.SiteNum,self.AppNum,self.EntityNum)
+        return struct.pack(self._fullFmt,self.SiteNum,self.AppNum,self.EntityNum)
 
     def __str__(self):
         return\
@@ -187,24 +220,24 @@ class EntityIdRecord(Rec):
         "EntityNum: %r" % (self.SiteNum,self.AppNum,self.EntityNum)
 
 '''class definition for Entity Type Record'''
-class EntityTypeRecord(Rec): 
-    Length = 8
+class EntityTypeRecord(Record): 
+    #Length = 8
     _entitymapper = None
     def __init__(self,packet=None,endianess='LITTLE',entitymapper=None):
-        Rec.__init__(self,packet,endianess)
+        Record.__init__(self,packet,endianess)
         if not self._entitymapper:
             EntityTypeRecord._entitymapper = entitymapper  #@remark - Is it ok to set class variable this way?.. i guess i could use class method
-        self._fmt = ENDIANESS[endianess] + 'BBHBBBB'
+        self._fmt = 'BBHBBBB'
         if not packet:
             self.Kind,self.Domain,self.Country,self.Category,self.SubCat,self.Specific,self.Extra = 0,0,0,0,0,0,0
             self.SimNames = None
         else:
             self.Kind,self.Domain,self.Country,self.Category,self.SubCat,self.Specific,self.Extra = \
-                struct.unpack_from(self._fmt,packet)
+                struct.unpack_from(self._fullFmt,packet)
             self.SimNames = None if not self._entitymapper else self._entitymapper.GetEntityNames([self.Kind,self.Domain,self.Country,self.Category,self.SubCat,self.Specific,self.Extra])
 
     def Pack(self):
-        return struct.pack(self._fmt,self.Kind,self.Domain,self.Country,self.Category,self.SubCat,self.Specific,self.Extra)
+        return struct.pack(self._fullFmt,self.Kind,self.Domain,self.Country,self.Category,self.SubCat,self.Specific,self.Extra)
 
     def __str__(self):
         return \
@@ -218,11 +251,48 @@ class EntityTypeRecord(Rec):
         "Extra: %r" % (None if not self.SimNames else self.SimNames,self.Kind,self.Domain,self.Country,self.Category,self.SubCat,self.Specific,self.Extra)
 
 '''class definition for Entity State PDU'''
-class EntityStatePdu(object):
+class EntityStatePdu(Record):
     _entitymapper = None
     def __init__(self,packet=None,endianess='LITTLE',entitymapper=None):
+        super(EntityStatePdu,self).__init__(packet,endianess)
         if not self._entitymapper:
             EntityStatePdu._entitymapper = entitymapper
+        if not packet:
+            self.PduHeader = PduHeaderRecord(endianess=endianess)
+            self.EntityId = EntityIdRecord(endianess=endianess)
+            self.ForceId = ForceIdRecord()
+            self.VarParamRecs = CustomRecord('B')
+            self.EntityType = EntityTypeRecord(endianess=endianess,entitymapper=entitymapper)
+        else:
+            self.PduHeader = PduHeaderRecord(packet,endianess)
+            packet = packet[self.PduHeader.Size():]  #@remark - best way i could think of for shifting bytes in packet over
+            self.EntityId = EntityIdRecord(packet,endianess)
+            packet = packet[self.EntityId.Size():]
+            self.ForceId = ForceIdRecord(packet)
+            packet = packet[self.ForceId.Size():]
+            self.VarParamRecs = CustomRecord('B',packet,endianess,lambda valLst : valLst[0])#  struct.unpack_from('B',packet)[0]
+            packet = packet[1:]
+            self.EntityType = EntityTypeRecord(packet,endianess,entitymapper)
+
+        self._fmt = ''.join([r._fmt for r in [self.PduHeader,self.EntityId,self.ForceId,self.VarParamRecs,self.EntityType]])
+
+    def Pack(self):
+        return self.PduHeader.Pack() + self.EntityId.Pack() + self.ForceId.Pack() \
+            + self.VarParamRecs.Pack() + self.EntityType.Pack()
+    
+    def __str__(self):
+        return\
+        "PDU Header:\n  %s\n"\
+        "Entity ID:\n  %s\n"\
+        "Force ID: %s\n"\
+        "Variable Parameter Records: %s\n"\
+        "Entity Type:\n  %s" % ('\n  '.join(str(self.PduHeader).splitlines()),
+                                '\n  '.join(str(self.EntityId).splitlines()), self.ForceId, self.VarParamRecs,
+                                '\n  '.join(str(self.EntityType).splitlines()))
+
+
+class SetDataPdu(object):
+    def __init__(self,packet=None,endianess='LITTLE'):        
         if not packet:
             self.PduHeader = PduHeaderRecord(endianess=endianess)
             self.EntityId = EntityIdRecord(endianess=endianess)
@@ -231,18 +301,18 @@ class EntityStatePdu(object):
             self.EntityType = EntityTypeRecord(endianess=endianess,entitymapper=entitymapper)
         else:
             self.PduHeader = PduHeaderRecord(packet,endianess)
-            packet = packet[PduHeaderRecord.Length:]  #@remark - best way i could think of for shifting bytes in packet over
+            packet = packet[self.PduHeader.Size():]  #@remark - best way i could think of for shifting bytes in packet over
             self.EntityId = EntityIdRecord(packet,endianess)
-            packet = packet[EntityIdRecord.Length:]
+            packet = packet[self.EntityId.Size():]
             self.ForceId = ForceIdRecord(packet)
-            packet = packet[ForceIdRecord.Length:]
+            packet = packet[self.ForceId.Size():]
             self.VarParamRecs = struct.unpack_from('B',packet)[0]
             packet = packet[1:]
             self.EntityType = EntityTypeRecord(packet,endianess,entitymapper)
 
     def Pack(self):
         return self.PduHeader.Pack() + self.EntityId.Pack() + self.ForceId.Pack() \
-            + struct.pack('B',self.VarParamRecs) + self.EntityType.Pack()
+            + self.VarParamRecs.Pack() + self.EntityType.Pack()
     
     def __str__(self):
         return\
--- test_dis.py
+++ test_dis.py
@@ -2,136 +2,135 @@
 
 import struct
 import binascii
-import pytest
+import unittest
 from DIS.utils import *
 from DIS.types import *
 
 
-def TestFullPackAndUnpack():
-    # buffer we will write to
-    buffer = ''
+class DisPacketTest(unittest.TestCase):
 
-    #Setup 96 bit/ 12 byte PDU header
-    protocolVer = 5   #random protocol version   8-bits
-    exId = 77         #random excercise id       8-bits
-    pduType = 28      #random pdu type           8-bits
-    protocolFam = 7   #random protocol family    8-bits
-    timestamp = 77    #random timestamp          32-bits
-    length = 12       #random length             16-bits
-    pduStatus = 77    #random pdu status         8-bit
-    padding = 0       #padding                   8-bit
-    buffer += struct.pack('<BBBBIHBB',protocolVer,exId,pduType,protocolFam,timestamp,length,pduStatus,padding)
+    def testFullPackAndUnpack(self):
+        # buffer we will write to
+        buffer = ''
 
-    #Setup 48 bit / 6 byte Entity Id
-    siteNum = 77      #site number 16bit u-int
-    appNum = 77       #application number 16bit u-int
-    entityNum = 77    #entity number 16bit u-int
-    buffer += struct.pack('<HHH',siteNum,appNum,entityNum)
+        #Setup 96 bit/ 12 byte PDU header
+        protocolVer = 5   #random protocol version   8-bits
+        exId = 77         #random excercise id       8-bits
+        pduType = 28      #random pdu type           8-bits
+        protocolFam = 7   #random protocol family    8-bits
+        timestamp = 77    #random timestamp          32-bits
+        length = 12       #random length             16-bits
+        pduStatus = 77    #random pdu status         8-bit
+        padding = 0       #padding                   8-bit
+        buffer += struct.pack('<BBBBIHBB',protocolVer,exId,pduType,protocolFam,timestamp,length,pduStatus,padding)
 
-    #setup 8bit/1byte force id record
-    forceId = 7       #force ID  8-bit
-    buffer += struct.pack('<B',forceId)
+        #Setup 48 bit / 6 byte Entity Id
+        siteNum = 77      #site number 16bit u-int
+        appNum = 77       #application number 16bit u-int
+        entityNum = 77    #entity number 16bit u-int
+        buffer += struct.pack('<HHH',siteNum,appNum,entityNum)
 
-    #setup 8bit/1byte number of variable parameter record field
-    varParams = 77     # 8-bits
-    buffer += struct.pack('<B',varParams)
+        #setup 8bit/1byte force id record
+        forceId = 7       #force ID  8-bit
+        buffer += struct.pack('<B',forceId)
 
-    #setup 64bit / 8byte entity type record
-    kind = 2         # 8-bit
-    domain = 7       # 8-bit
-    country = 12      # 16-bit
-    cat = 17          # 8-bit
-    subCat = 22       # 8-bit
-    spec = 27         # 8-bit
-    extra = 32        # 8-bit
-    buffer += struct.pack('<BBHBBBB',kind,domain,country,cat,subCat,spec,extra)
+        #setup 8bit/1byte number of variable parameter record field
+        varParams = 77     # 8-bits
+        buffer += struct.pack('<B',varParams)
 
-    entMapper = DisEntityMapper('EntityEnumerations.txt')
-    ePdu = EntityStatePdu(packet=buffer,entitymapper=entMapper)
+        #setup 64bit / 8byte entity type record
+        kind = 2         # 8-bit
+        domain = 7       # 8-bit
+        country = 12      # 16-bit
+        cat = 17          # 8-bit
+        subCat = 22       # 8-bit
+        spec = 27         # 8-bit
+        extra = 32        # 8-bit
+        buffer += struct.pack('<BBHBBBB',kind,domain,country,cat,subCat,spec,extra)
+
+        entMapper = DisEntityMapper('EntityEnumerations.txt')
+        ePdu = EntityStatePdu(packet=buffer,entitymapper=entMapper)
     
-    print 'Testing Entity State Pdu Unpacking'
+        print 'Testing Entity State Pdu Unpacking'
 
-    assert ePdu.PduHeader.ProtocolVersion == protocolVer
-    assert ePdu.PduHeader.ExerciseId == exId
-    assert ePdu.PduHeader.PduType == pduType
-    assert ePdu.PduHeader.ProtocolFamily == protocolFam
-    assert ePdu.PduHeader.Timestamp == timestamp
-    assert ePdu.PduHeader.TotalLength == length
-    assert ePdu.PduHeader.PduStatus.Value == pduStatus
-    assert ePdu.EntityId.SiteNum == siteNum
-    assert ePdu.EntityId.AppNum == appNum
-    assert ePdu.EntityId.EntityNum == entityNum
-    assert ePdu.ForceId.Value == forceId
-    assert ePdu.VarParamRecs == varParams
-    assert ePdu.EntityType.Kind == kind
-    assert ePdu.EntityType.Domain == domain
-    assert ePdu.EntityType.Country == country
-    assert ePdu.EntityType.Category == cat
-    assert ePdu.EntityType.SubCat == subCat
-    assert ePdu.EntityType.Specific == spec
-    assert ePdu.EntityType.Extra == extra
+        self.assertEqual(ePdu.PduHeader.ProtocolVersion,protocolVer)
+        self.assertEqual(ePdu.PduHeader.ExerciseId,exId)
+        self.assertEqual(ePdu.PduHeader.PduType,pduType)
+        self.assertEqual(ePdu.PduHeader.ProtocolFamily,protocolFam)
+        self.assertEqual(ePdu.PduHeader.Timestamp,timestamp)
+        self.assertEqual(ePdu.PduHeader.TotalLength,length)
+        self.assertEqual(ePdu.PduHeader.PduStatus._value,pduStatus)
+        self.assertEqual(ePdu.EntityId.SiteNum,siteNum)
+        self.assertEqual(ePdu.EntityId.AppNum,appNum)
+        self.assertEqual(ePdu.EntityId.EntityNum,entityNum)
+        self.assertEqual(ePdu.ForceId.Value,forceId)
+        self.assertEqual(ePdu.VarParamRecs.Value,varParams)
+        self.assertEqual(ePdu.EntityType.Kind,kind)
+        self.assertEqual(ePdu.EntityType.Domain,domain)
+        self.assertEqual(ePdu.EntityType.Country,country)
+        self.assertEqual(ePdu.EntityType.Category,cat)
+        self.assertEqual(ePdu.EntityType.SubCat,subCat)
+        self.assertEqual(ePdu.EntityType.Specific,spec)
+        self.assertEqual(ePdu.EntityType.Extra,extra)
 
-    print 'Testing Entity State Pdu Packing'
-    retBuffer = ePdu.Pack()
-    assert buffer == retBuffer
+        print 'Testing Entity State Pdu Packing'
+        retBuffer = ePdu.Pack()
+        self.assertEqual(buffer,retBuffer)
 
-    return ePdu
+        print ePdu
 
 
-def TestPduHeaderRecord():
-    #test protocolversion range
-    tmp = PduHeaderRecord()
-    failRange = [x for x in range(256) if x not in PduHeaderRecord.protocolVersionRange]
-    print 'Testing PduHeader.ProtocolVersion for failure of %d values' % len(failRange)
-    for i in failRange:
-        with pytest.raises(ValueError):
-            tmp.ProtocolVersion = i
+    def testPduHeaderRecord(self):
+        #test protocolversion range
+        tmp = PduHeaderRecord()
+        failRange = [x for x in range(256) if x not in PduHeaderRecord.protocolVersionRange]
+        print 'Testing PduHeader.ProtocolVersion for failure of %d values' % len(failRange)
+        for i in failRange:
+            with self.assertRaises(ValueError):
+                tmp.ProtocolVersion = i
 
-    failRange = [x for x in range(256) if x not in PduHeaderRecord.pduTypeRange]
-    print 'Testing PduHeader.PduType for failure of %d values' % len(failRange)
-    for i in failRange:
-        with pytest.raises(ValueError):
-            tmp.PduType = i
+        failRange = [x for x in range(256) if x not in PduHeaderRecord.pduTypeRange]
+        print 'Testing PduHeader.PduType for failure of %d values' % len(failRange)
+        for i in failRange:
+            with self.assertRaises(ValueError):
+                tmp.PduType = i
 
-    failRange = [x for x in range(256) if x not in PduHeaderRecord.protocolFamilyRange]
-    print 'Testing PduHeader.ProtocolFamily for failure of %d values' % len(failRange)
-    for i in failRange:
-        with pytest.raises(ValueError):
-            tmp.ProtocolFamily = i
+        failRange = [x for x in range(256) if x not in PduHeaderRecord.protocolFamilyRange]
+        print 'Testing PduHeader.ProtocolFamily for failure of %d values' % len(failRange)
+        for i in failRange:
+            with self.assertRaises(ValueError):
+                tmp.ProtocolFamily = i
 
-def TestPduStatusRecord():
-    packet = struct.pack('B',0xFF)
-    print 'Testing PduStatusRecord to make sure it cant be initialized with zero as pdutype'
-    with pytest.raises(ValueError):
-        tmp = PduStatusRecord(0,packet)
-    print 'Testing PduStatusRecord to make sure TEI,LVC,CEI,FTI,DTI,RAI,IAI,ISM,and AII\nvalues are correct for each pdutype'
-    tmp = PduStatusRecord(1,packet)
-    for k,v in PduStatusRecord._rules.items():
-        for i in range(1,256):
-            tmp.PduType = i
-            val = getattr(tmp,k)
-            if i in v:
-                assert val
+    def testPduStatusRecord(self):
+        packet = struct.pack('B',0xFF)
+        print 'Testing PduStatusRecord to make sure it cant be initialized with zero as pdutype'
+        with self.assertRaises(ValueError):
+            tmp = PduStatusRecord(0,packet)
+        print 'Testing PduStatusRecord to make sure TEI,LVC,CEI,FTI,DTI,RAI,IAI,ISM,and AII\nvalues are correct for each pdutype'
+        tmp = PduStatusRecord(1,packet)
+        for k,v in PduStatusRecord._rules.items():
+            for i in range(1,256):
+                tmp.PduType = i
+                val = getattr(tmp,k)
+                if i in v:
+                    self.assertIsNotNone(val)
 
-def TestLookupMapping():
-    #setup table to match values in enumerations config file
-    table = []
-    for x in range(1,6):
-        table.append([x + y*5 for y in range(0,7)])
-    #setup mapper
-    entMapper = DisEntityMapper('EntityEnumerations.txt')
-    print 'Testing DisEntityMapper with %d values' % len(table)
-    for vals in table:
-        assert entMapper.GetEntityNames(vals)
+    def testLookupMapping(self):
+        #setup table to match values in enumerations config file
+        table = []
+        for x in range(1,6):
+            table.append([x + y*5 for y in range(0,7)])
+        #setup mapper
+        entMapper = DisEntityMapper('EntityEnumerations.txt')
+        print 'Testing DisEntityMapper with %d values' % len(table)
+        for vals in table:
+            self.assertIsNotNone(entMapper.GetEntityNames(vals))
+
+
 
 
 if __name__ == '__main__':
     print
     print '*** Starting Tests ***'
-    TestPduHeaderRecord()
-    TestPduStatusRecord()
-    TestLookupMapping()
-    fullPdu = TestFullPackAndUnpack()
-    print 'Test str override of Entity State PDU\n'
-    print fullPdu
+    unittest.main()
 
